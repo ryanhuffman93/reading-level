46365061334dd4f18c811ba59088182c6530c924
Ryan Huffman <ryanhuffman93@gmail.com>
2021-07-02 17:34:04 -0400

    Add main script

---
+++ read.py
@@ -0,0 +1,50 @@
+from cs50 import get_string
+
+def main():
+    string = get_string("Text: ")
+    letters = count_letters(string)
+    words = count_words(string)
+    sentences = count_sentences(string)
+
+    L = letters * (100 / words)
+    S = sentences * (100 / words)
+
+    grade = round(0.0588 * L - 0.296 * S - 15.8)
+    
+    if grade < 1:
+        print("\nBefore Grade 1")
+    elif grade > 15:
+        print("\nGrade 16+")
+    else:
+        print(f"\nGrade {grade}")
+        
+    print(f"\nLetters: {letters}\nWords: {words}\nSentences: {sentences}\n")
+
+
+def count_letters(string):
+    letters = 0
+    for char in string:
+        if (char.isalpha()) == True:
+            letters += 1
+    return letters
+
+def count_words(string):
+    words = 1
+    prev_char = ""
+    for char in string:
+        if (char != " ") and (prev_char == " "):
+            words += 1
+        prev_char = char
+    return words
+
+def count_sentences(string):
+    punctuation = [".", "?", "!"]
+    sentences = 1
+    prev_char = ""
+    for char in string:
+        if (char not in punctuation) and (char != '"') and (prev_char in punctuation):
+            sentences += 1
+        prev_char = char
+    return sentences
+
+main()
